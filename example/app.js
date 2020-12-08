e622f77def710f5149e8bd9ad9559dfc8744e5a8
kamataryo <kamataryo@users.noreply.github.com>
2020-12-08 21:49:54 +0900

    Fix example close #2

--- example/app.js
+++ example/app.js
@@ -1,19 +1,6 @@
 import ExportControl from '..'
 
-const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'https://tilecloud.github.io/tiny-tileserver/style.json',
-  attributionControl: true,
-  hash: true,
-  localIdeographFontFamily: ['sans-serif']
-});
-
-map.addControl(new mapboxgl.NavigationControl())
-map.addControl(new ExportControl({
-  dpi: 300,
-}))
-
-const tilecloudMap = new tilecloud.Map(document.querySelector('#tilecloud-custom'))
-tilecloudMap.addControl(new ExportControl({
+const geoloniaMap = new geolonia.Map('#geolonia')
+geoloniaMap.addControl(new ExportControl({
   dpi: 300,
 }))
